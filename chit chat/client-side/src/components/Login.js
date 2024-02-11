ee5cd8aa310c6a7af11e2797137a5fd8c10a27bf
Hashan Malinda <85447795+hashan789@users.noreply.github.com>
2024-02-11 22:04:01 +0530

    Update Login.js

--- chit chat/client-side/src/components/Login.js
+++ chit chat/client-side/src/components/Login.js
@@ -18,17 +18,17 @@ export default function Login({ onIdSubmit }) {
 
     return (
         <Container className="d-flex align-items-center justify-content-center border p-4" style={{marginTop:'200px'}}>
-          <div className="d-flex mx-auto">
-            <div className="d-flex mx-auto">
+          <div className="mx-auto">
+            <div className="d-flex justify-content-center mx-auto">
                 <h6><b style={{ fontSize : '50px' }}>Chit Chat</b></h6>
             </div>
             <Form onSubmit={handleSubmit}>
                 <Form.Group className="d-flex align-items-center mx-auto">
-                    <Form.Label style={{width:'200px'}}>Enter your Id</Form.Label>
-                     <div style={{width:'750px',marginLeft:'20px'}}>
-                       <Form.Control type="text" ref={idRef} style={{width:'600px'}} required/>
-                       <Button type="submit" className="m-4">Login</Button>
-                       <Button onClick={createNewId} variant="secondary">Create A New Id</Button>
+                     <div style={{width:'750px'}}>
+                       <Form.Label style={{width:'200px'}}>Enter your Id</Form.Label>
+                       <Form.Control type="text" ref={idRef} style={{width:'100%'}} required/>
+                       <Button type="submit" className="mt-4 mb-4">Login</Button>
+                       <Button className="mx-4" onClick={createNewId} variant="secondary">Create A New Id</Button>
                      </div>
                 </Form.Group>
             </Form>
