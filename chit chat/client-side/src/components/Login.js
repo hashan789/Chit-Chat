ac6c2bca9b49472ec183afa92d370b1e50de1f89
Hashan Malinda <85447795+hashan789@users.noreply.github.com>
2024-02-11 20:40:08 +0530

    Update Login.js

--- chit chat/client-side/src/components/Login.js
+++ chit chat/client-side/src/components/Login.js
@@ -18,9 +18,12 @@ export default function Login({ onIdSubmit }) {
 
     return (
         <Container className="d-flex align-items-center justify-content-center border p-4" style={{marginTop:'200px'}}>
-          <div>
+          <div className="d-flex mx-auto">
+            <div className="d-flex mx-auto">
+                <h6><b style={{ fontSize : '50px' }}>Chit Chat</b></h6>
+            </div>
             <Form onSubmit={handleSubmit}>
-                <Form.Group className="d-flex align-items-center">
+                <Form.Group className="d-flex align-items-center mx-auto">
                     <Form.Label style={{width:'200px'}}>Enter your Id</Form.Label>
                      <div style={{width:'750px',marginLeft:'20px'}}>
                        <Form.Control type="text" ref={idRef} style={{width:'600px'}} required/>
