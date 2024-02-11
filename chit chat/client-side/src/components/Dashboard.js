8d3aeb7310c120a2a62f5226567f2e9d984fba6b
Hashan Malinda <85447795+hashan789@users.noreply.github.com>
2024-02-11 19:18:47 +0530

    Update Dashboard.js

--- chit chat/client-side/src/components/Dashboard.js
+++ chit chat/client-side/src/components/Dashboard.js
@@ -8,7 +8,7 @@ export default function Dashboard({ id }) {
   const { selectedChatbox } = useChatbox()
   
   return (
-        <div className="d-flex">
+        <div className="d-flex" style={{ height : '100vh' }}>
           <Sidebar id={id}/>
           {selectedChatbox && <Chat />}
         </div>
